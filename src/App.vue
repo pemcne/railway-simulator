fe729bd0c7c5a21756439d9278741e5bd61bccfd
pemcne <pemcneill8@gmail.com>
2018-05-31 00:30:02 -0400

    Intial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Market/>
+  </div>
+</template>
+
+<script>
+import Market from './components/Market'
+
+export default {
+  name: 'App',
+  components: {
+    Market
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Market.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="market">
+    <ul>
+      <li>Timber - $2</li>
+      <li>Steel - $5</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Market'
+}
+</script>
+
+<style>
+#market {
+  border: 1px black solid;
+}
+</style>
