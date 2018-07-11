2a7eee49a70687ddda2901d5d168cd133d2bfe46
pemcne <pemcneill8@gmail.com>
2018-07-11 00:03:21 -0400

    Stub out a train component

---
+++ src/components/Train.vue
@@ -0,0 +1,18 @@
+<template>
+</template>
+
+<script>
+import EventBus from '@/modules/EventBus'
+export default {
+  name: 'Train',
+  props: ['trainId'],
+  computed: {},
+  methods: {
+    move (scale) {
+      // move the train
+    }
+  },
+  mounted () {
+    EventBus.$on('tick', (scale) => this.move(scale))
+  }
+}
